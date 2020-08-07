fc42cc4de7a826a1b00ecdd5ac949758a26d9782
Shruthi Rao <raoshruthi2001@gmail.com>
2020-08-07 18:02:23 +0530

    update models.py - corrected typo

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ def user_loader(id):
 
 from flask_login import UserMixin
 
-class User(UserMixin, db.model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
